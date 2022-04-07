93b5075ae556bb148575ce1793b27a8e7c406d6f
0x0c0d03edf4e145ffd75eafb6d73deaa7 <103005750+0x0c0d03edf4e145ffd75eafb6d73deaa7@users.noreply.github.com>
2022-04-07 15:13:55 +0200

    Update bot.spec.js

--- cypress/integration/bot.spec.js
+++ cypress/integration/bot.spec.js
@@ -1,25 +1,32 @@
-// untitled.spec.js created with Cypress
-
 describe('My First Test', () => {
     it('Does not do much!', () => {
-        for (let i = 0; i <= 24; i++) {
+        for (let i = 0; i <= 1000; i++) {
+            cy.visit('https://soundcloud.com/lydiaeisenblaetter/lydia-eisenblatter-live-robert-johnson-part-2', {
+                headers: {
+                    'user-agent': 'Opera/7.0 ' + Math.random(),
+                    'referrer': 'https://www.twitter.com',
+                }
+            })
+
+            cy.wait(20000 + Math.random() * 2000)
+
+            cy.visit('https://soundcloud.com/lydiaeisenblaetter/lydia-eisenblatter-live-robert-johnson-part-1', {
+                headers: {
+                    'user-agent': 'Opera/7.0 ' + Math.random(),
+                    'referrer': 'https://www.twitter.com',
+                }
+            })
+
+            cy.wait(20000 + Math.random() * 2000)
+
             cy.visit('https://soundcloud.com/julianewolfmusic/juliane-wolf-sos-uground-kulturkosmos-larz-02042022', {
                 headers: {
                     'user-agent': 'Opera/7.0 ' + Math.random(),
-                    'referrer': 'https://www.instagram.com',
+                    'referrer': 'https://www.twitter.com',
                 }
             })
 
-            cy.wait(38000 + Math.random() * 4000)
-            
-            cy.visit('https://soundcloud.com/julianewolfmusic/juliane-wolf-dj-set-wilde-mohre-festival-2021-firletanz-wildschreck-17-07-2021?in=julianewolfmusic/sets/dj-sets', {
-                headers: {
-                    'user-agent': 'Opera/7.0 ' + Math.random(),
-                    'referrer': 'https://www.instagram.com',
-                }
-            })
-            
-            cy.wait(38000 + Math.random() * 4000)
+            cy.wait(20000 + Math.random() * 2000)
         }
     })
 })
