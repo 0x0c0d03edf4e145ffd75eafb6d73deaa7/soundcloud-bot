dd41962e295180989eafd1ec89f4b7dc840e9711
0x0c0d03edf4e145ffd75eafb6d73deaa7 <103005750+0x0c0d03edf4e145ffd75eafb6d73deaa7@users.noreply.github.com>
2022-04-13 11:41:32 +0200

    Update bot.spec.js

--- cypress/integration/bot.spec.js
+++ cypress/integration/bot.spec.js
@@ -2,7 +2,7 @@ describe('My First Test', () => {
     it('Does not do much!', () => {
         for (let i = 0; i <= 29; i++) {
 
-            cy.visit('https://soundcloud.com/julianewolfmusic/juliane-wolf-sos-uground-kulturkosmos-larz-02042022', {
+            cy.visit('https://soundcloud.com/dilivius-lenni/dilivius-lenni-27-jahre-distillery-leipzig-19102019-24h-wohnzimmer-dj-set', {
                 headers: {
                     'user-agent': 'Opera/7.0 ' + Math.random(),
                     'referrer': 'https://www.facebook.com',
