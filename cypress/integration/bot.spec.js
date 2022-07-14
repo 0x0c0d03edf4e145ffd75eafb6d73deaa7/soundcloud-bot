7d88d5f6f8d470714987f1b98f922faeb2adc500
0x0c0d03edf4e145ffd75eafb6d73deaa7 <103005750+0x0c0d03edf4e145ffd75eafb6d73deaa7@users.noreply.github.com>
2022-07-14 11:13:22 +0200

    Update bot.spec.js

--- cypress/integration/bot.spec.js
+++ cypress/integration/bot.spec.js
@@ -2,6 +2,7 @@ describe('My First Test', () => {
     it('Does not do much!', () => {
         for (let i = 0; i <= 26; i++) {
             //cy.visit('https://soundcloud.com/mariemontexier/marie-montexier-turmbuhne-at-fusion-festival-2022', {
+            cy.visit('https://soundcloud.com/andreas-henneberg/andreas-henneberg-at-fusion-festival-2022-turmbuhne', {
                 headers: {
                     'user-agent': 'Opera/7.0 ' + Math.random(),
                     'referrer': 'https://www.facebook.com',
